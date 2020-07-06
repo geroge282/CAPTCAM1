7a5f0c2a995ac4315e47e9666db0d5f591b99a80
jorge <jorge.gueo28@hotmail.com>
2020-07-06 17:02:33 -0500

    PRIMERA CREACION

---
+++ app/src/main/java/com/example/captcam1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.captcam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnIngresar(View v){
+
+        Intent intent=new Intent(v.getContext(), usuario.class);
+        startActivityForResult(intent,0);
+
+    }
+    public void BotonRegistro(View v){
+
+        Intent intent=new Intent(v.getContext(), Registro.class);
+        startActivityForResult(intent,0);
+
+    }
+    public void BotonSalir(View v){
+
+        Intent intent=new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/captcam1/captura_rostro.java
@@ -0,0 +1,14 @@
+package com.example.captcam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class captura_rostro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_captura_rostro);
+    }
+}
