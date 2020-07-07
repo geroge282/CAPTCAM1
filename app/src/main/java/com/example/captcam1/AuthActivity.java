33419e9211105f3a3892b7bdadd74d8bc369a020
jorge <jorge.gueo28@hotmail.com>
2020-07-06 21:56:32 -0500

    atentificacion

--- app/src/main/java/com/example/captcam1/AuthActivity.java
+++ app/src/main/java/com/example/captcam1/AuthActivity.java
@@ -6,15 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class AuthActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        mAuth = FirebaseAuth.getInstance();
+
+
     }
 
+
+
     public void btnIngresar(View v){
+
 
         Intent intent=new Intent(v.getContext(), homeUsuario.class);
         startActivityForResult(intent,0);
